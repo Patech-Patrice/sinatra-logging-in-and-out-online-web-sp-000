002eafa1982a457f692025b601524059a0af6003
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-24 02:13:55 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,7 @@
 class Helpers
   def self.current_user(session)
-    User.find(session[:user_id])
+    current_user = User.find(session[:user_id])
+    current_user
   end
 
   def self.is_logged_in?(session)
